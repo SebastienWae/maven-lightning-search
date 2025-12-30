9f9bfdf5b77aff2a5e8cdd39fcc70b224fd45370
Sebastien Waegeneire <sebastien@waegeneire.com>
2025-12-30 15:31:13 +0100

    add color to icon

--- src/web/src/components/Header.tsx
+++ src/web/src/components/Header.tsx
@@ -8,7 +8,7 @@ export function Header() {
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
         <Separator orientation="vertical" />
-        <LightningIcon />
+        <LightningIcon weight="fill" className="text-primary" />
         <Separator orientation="vertical" />
         <h1 className="text-base font-medium select-none">Maven Lightning Search</h1>
         <Separator orientation="vertical" />
